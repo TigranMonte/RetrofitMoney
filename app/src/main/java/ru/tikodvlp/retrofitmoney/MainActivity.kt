f8dcdab0f2e148f6ceecf07c5f1938a4339855f4
Tigran <tigranma@gmail.com>
2022-12-07 17:37:50 +0300

    first commit

---
+++ app/src/main/java/ru/tikodvlp/retrofitmoney/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.tikodvlp.retrofitmoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
